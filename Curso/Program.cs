d7d3885689d339832070f5414a0536c46969394a
Abraão dos santos batista <abraas898@gmail.com>
2022-09-09 22:09:39 -0300

    Entrada de Dados em C# Parte 1. Entrada de Dados em C# Parte 2

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -7,25 +7,24 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            int n1 = 3 + 4 * 2;
-            int n2 = (3 + 4) * 2;
-            int n3 = 17 % 3;
-            double n4 = 10.0 / 8;
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-            double a = 1.0, b = -3.0, c = -4.0;
+            string[] vet = Console.ReadLine().Split(' ');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
-
-            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
-            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
-
+            Console.WriteLine("voce digitou");
             Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.WriteLine(n3);
-            Console.WriteLine(n4);
-            Console.WriteLine(delta);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("f2",CultureInfo.InvariantCulture));
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
