6d6f5abad337d62b02e51982d9a18f3583304f23
Abraão dos santos batista <abraas898@gmail.com>
2022-09-14 21:06:35 -0300

    Estrutura Repetitiva enquanto (while). Estrutura Repetitiva Para (For)

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -7,33 +7,18 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("digite tres números:");
-            int n1 = int.Parse(Console.ReadLine());
-            int n2 = int.Parse(Console.ReadLine());
-            int n3 = int.Parse(Console.ReadLine());
+            Console.Write("quantos numeros voce vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
 
-            double resultado = Maior(n1, n2, n3);
-
-            Console.WriteLine("Maior = " + resultado);
-        }
-
-
-        static int Maior(int a, int b, int c)
-        {
-            int m;
-            if (a > b && a > c)
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
             {
-                m = a;
+                Console.WriteLine("valor #{0}: ",i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
             }
-            else if (b > c)
-            {
-                m = b;
-            }
-            else
-            {
-                m = c;
-            }
-            return m;
+
+            Console.WriteLine("Soma = " + soma);
         }
     }
 }
