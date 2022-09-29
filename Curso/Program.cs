5420655e2d50701b3f4bdb48bf6a60be811ba192
Abraão dos santos batista <abraas898@gmail.com>
2022-09-29 19:33:23 -0300

    Seção 6: aula 68, Nullable.

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -6,12 +6,24 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            Point p;
-            p.x = 10;
-            p.y = 20;
-            Console.WriteLine(p);
-            p = new Point();
-            Console.WriteLine(p);
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault ());
+            Console.WriteLine(y.GetValueOrDefault ());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+                 Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("x is null");
+
+            if(y.HasValue)
+                 Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("y is null");
         }
     }
 }
