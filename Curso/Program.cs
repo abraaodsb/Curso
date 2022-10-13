81b315e809fa10958d97bab80f2662f99f738907
Abraão dos santos batista <abraas898@gmail.com>
2022-10-13 16:38:17 -0300

    Seção 7: TimeSpan - representando durações

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Globalization;
-using System.Collections.Generic;
 
 namespace Curso
 {
@@ -8,15 +6,21 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            DateTime d1 = DateTime.ParseExact("2000-08-15", "yyyy-mm-dd", CultureInfo.InvariantCulture );
+            TimeSpan t1 =  TimeSpan.FromDays(1.5);
+            TimeSpan t2 =  TimeSpan.FromHours(1.5);
+            TimeSpan t3 =  TimeSpan.FromMinutes(1.5);
+            TimeSpan t4 =  TimeSpan.FromSeconds(1.5);
+            TimeSpan t5 =  TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t6 =  TimeSpan.FromTicks(900000000L);
 
-            DateTime d2 = DateTime.ParseExact("2000-08-15 13:05:58", "dd/mm/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
 
-           
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+            Console.WriteLine(t6);
 
-            Console.WriteLine(d1);
-            Console.WriteLine(d2);
-            
         }
     }
 }
