08739fa4de923861fc737242614073f2b9206125
Abraão dos santos batista <abraas898@gmail.com>
2022-10-18 19:20:07 -0300

    Seção 10: Herança)

---
+++ Curso/Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Entities
+{
+    class BusinessAccount : Account 
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount (int number, string holder, double balance, double loanLimit)  
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
