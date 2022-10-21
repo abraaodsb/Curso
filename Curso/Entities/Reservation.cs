ac804beeafea6cb3ab92b53b41b22a16df53665a
Abraão dos santos batista <abraas898@gmail.com>
2022-10-21 20:04:49 -0300

    Seção 11: Criando exceções personalizadas parte 1

---
+++ Curso/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Entities
+{
+    class Reservation
+    {
+        public int RoonNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roonNumber, DateTime checkin, DateTime checkOut)
+        {
+            RoonNumber = roonNumber;
+            CheckIn = checkin;
+            CheckOut = checkOut;
+        }
+         public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void updateDates(DateTime checkIn, DateTime  checkOut)
+        {
+            CheckIn = CheckIn;
+            CheckOut = CheckOut;
+        }
+        public override string ToString()
+        {
+            return "Room"
+                + RoonNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + "nights";
+        }
+    }
+}
--- Curso/Program.cs
+++ Curso/Program.cs
@@ -11,21 +11,39 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            try
-            {
-                int n1 = int.Parse(Console.ReadLine());
-                int n2 = int.Parse(Console.ReadLine());
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy):");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-Out date (dd/MM/yyyy):");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
 
-                int result = n1 / n2;
-                Console.WriteLine(result);
-            }
-            catch(DivideByZeroException )
+            if (checkOut <= checkIn)
             {
-                Console.WriteLine("Division by zero is not allowed");
+                Console.WriteLine("Error in reservation: checkOut date must be after check-in");
             }
-            catch (FormatException e)
+            else
             {
-                Console.WriteLine("Format error! " + e.Message);
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy):");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-Out date (dd/MM/yyyy):");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
+                {
+                    Console.WriteLine("Error in reservation dates for update must be future dates");
+                }
+                else
+                {
+                    reservation.updateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservatiom: " + reservation);
+                }
             }
         }
     }
