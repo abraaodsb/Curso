51f9b332806f90ae6e4db62de068665f613db7bd
Abraão dos santos batista <abraas898@gmail.com>
2022-11-24 20:39:19 -0300

    Secao 13:FileStream, e StreamReader

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -12,7 +12,7 @@ namespace Curso
 {
     class Program
     {
-        static void Main(string[] args, object fileInfo)
+         static void Main(string[] args)
         {
             #region Program antigo
             //try
@@ -49,24 +49,27 @@ namespace Curso
 
 
             #region File/FileInfo/Exception
-            string sourcePath = @"c:\temp\file1.txt";
-            string targetPath = @"c:\temp\file2.txt";
+            string path = @"c:\temp\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
 
             try
             {
-                FileInfo fileI = new FileInfo(sourcePath);
-                //fileInfo,CopyTo(targetPath);//
-                string[] lines = File.ReadAllLines(sourcePath);
-                foreach (string line in lines)
-                {
-                    Console.WriteLine(line);
-                }
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
             }
             catch (IOException e)
             {
-
-                Console.WriteLine("An error ocurred");
+                Console.WriteLine("An error accurred");
                 Console.WriteLine(e.Message);
+               
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                if (fs != null) fs.Close();
             }
             #endregion          
         }
