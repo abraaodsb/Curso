ba3a366a8dda344802f34431871f1b7ebaf27ac1
Abraão dos santos batista <abraas898@gmail.com>
2023-02-03 20:27:01 -0300

    Finalizado sobre Entities da seção 14

---
+++ Curso/Entities/Invoice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Curso.Entities
+{
+    class Invoice
+    {
+        public double basicPayment { get; set; }
+        public double tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            this.basicPayment = basicPayment;
+            this.tax = tax;
+        }
+
+        public double TotalPayment {
+            get { return basicPayment + tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: " 
+                +  basicPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Curso/Entities/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.Entities
+{
+    class Vehicle
+    {
+        public string model { get; set; }
+
+        public Vehicle(string model)
+        {
+            this.model = model;
+        }
+    }
+}
